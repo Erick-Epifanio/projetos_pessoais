ce466d42dcfd94d7ea2f2c69b7e3967862a0f94a
VeilCruss <erickepifaniodasilva@gmail.com>
2025-07-31 19:44:48 -0300

    teste

--- equal.py
+++ equal.py
@@ -1,5 +1,6 @@
 import math
 import os
+import termcolor
 
 
 def Insert_data():
@@ -10,15 +11,16 @@ def Insert_data():
             b = float(int(input(" B: "))) or 1
             c = float(int(input(" C: "))) or 1
 
+
             if input(f"\n Valores inseridos:\n| {a}\n| {b}\n| {c}\n\n Deseja continuar ou refazer? S/N: ").upper() == "S":
                 run = False
                 return a, b, c
             else:
-                raise Exception(" resetando....")
+                raise Exception(termcolor.colored(" resetando....", "yellow"))
         except Exception:
-            os.system("clear")
+            os.system("cls")
         except ValueError:
-            os.system("clear")
+            os.system("cls")
             print("Erro de inserção")
 
 def Equal_Delta(a, b, c):
@@ -28,14 +30,23 @@ def Equal_Delta(a, b, c):
 def Equal_geral(a, b, c, Delta):
     
     def x_mais():
-        x1 = (-1*b) + math.sqrt(Delta)
-        x1 = x1 / (2 * a)
-        return x1
+        try:
+            x1 = (-1*b) + math.sqrt(Delta)
+            x1 = x1 / (2 * a)
+            return x1
+        except ValueError:
+            print(termcolor.colored(f"Erro de operação envolvendo 'Raiz quadrada' com {Delta}", "yellow"))
+            return termcolor.colored("XXXX", "red")
         
     def x_menos():
-        x2 = (-1*b) - math.sqrt(Delta)
-        x2 = x2 / (2 * a)
-        return x2
+        try:
+            x2 = (-1*b) - math.sqrt(Delta)
+            x2 = x2 / (2 * a)
+            return x2
+        except ValueError:
+            print(termcolor.colored(f"Erro de operação envolvendo 'Raiz quadrada' com {Delta}", "yellow"))
+            return termcolor.colored("XXXX", "red")
+        
     x1 = x_mais()
     x2 = x_menos()   
     print(f"x': {x1}")
@@ -44,7 +55,7 @@ def Equal_geral(a, b, c, Delta):
 
 while True:
     input("Pressione Enter para continuar...")
-    os.system("clear")
+    os.system("cls")
     a, b, c = Insert_data()
     Delta = Equal_Delta(a, b, c)
     Equal_geral(a, b, c, Delta)
